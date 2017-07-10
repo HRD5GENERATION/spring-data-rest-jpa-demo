d39a2f7efcec4d332f67ed86df681b706de37108
Phearun Rath <rathphearun168@gmail.com>
2017-07-10 20:19:12 +0700

    added: one search method

--- src/main/java/com/phearun/repository/ArticleRepository.java
+++ src/main/java/com/phearun/repository/ArticleRepository.java
@@ -1,11 +1,16 @@
 package com.phearun.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.phearun.model.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer>{
+
+	List<Article> findByTitleIgnoreCaseContaining(@Param("title") String title);
 
 }
